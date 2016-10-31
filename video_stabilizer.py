8c61b4ce3ddf9dad7c1b89dd993bb418205678c5
Ace2196 <abdulla.contractor@gmail.com>
2016-10-31 19:41:17 +0800

    some aesthetic things

--- video_stabilizer.py
+++ video_stabilizer.py
@@ -3,6 +3,7 @@ import argparse
 import imutils
 import cv2
 import os
+import progressbar
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,14 +19,17 @@ if not os.path.exists(VIDEO_FRAME_DIR):
 stitcher = Stitcher()
 
 cap = cv2.VideoCapture(args["video"])
+fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
 frame_count = cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+print('To stitch frames to create video, call:')
+print('ffmpeg -framerate %d -i %s%%d.jpg -c:v libx264 -profile:v high -crf 19 -pix_fmt yuv420p warped%s.mp4'%(fps,VIDEO_FRAME_DIR,VIDEO_NAME))
+bar = progressbar.ProgressBar(maxval=frame_count, \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
 _, frame_prev = cap.read()
 i=1
 while i < frame_count:
-	_, frame_i = cap.read()
-	i += 1
-	print('%d/%d'%(i,frame_count))
-	result = stitcher.stitch([frame_prev, frame_i], showMatches=False)
-	cv2.imwrite("%s%d.jpg"%(VIDEO_FRAME_DIR,i), result)
-
-# call(['ffmpeg -framerate 5 -i frame%d.jpg -c:v libx264 -profile:v high -crf 19 -pix_fmt yuv420p messi.mp4'])
+    bar.update(i)
+    _, frame_i = cap.read()
+    i += 1
+    result = stitcher.stitch([frame_prev, frame_i], showMatches=False)
+    cv2.imwrite("%s%d.jpg"%(VIDEO_FRAME_DIR,i), result)
