ed6918c11af3025f4ccc4a5595f45293f5cd9191
Benjamin Lim <bumbleblym@gmail.com>
2016-11-06 14:51:29 +0800

    fix equalize_color

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def equalize_color(img):
     channels = cv2.split(img_ycrcb)
     channels[0] = cv2.equalizeHist(channels[0])
 
-    img_equalized = cv2.merge(channels, img)
-    img_equalized = cv2.cvtColor(img, cv2.COLOR_YCR_CB2BGR)
+    img_equalized = cv2.merge(channels)
+    img_equalized = cv2.cvtColor(img_equalized, cv2.COLOR_YCR_CB2BGR)
 
     return img_equalized
