bc1ca9fe5b88ee1f892b1615ebd5b95d1b3fef45
Benjamin Lim <bumbleblym@gmail.com>
2016-11-12 06:14:53 +0800

    fix save homography

--- homography.py
+++ homography.py
@@ -76,7 +76,7 @@ def save():
         video = Video(video_pathname)
 
         video_filename = basename(video_pathname)
-        matrices_pathname = join(matrices_dirname, video_filename + '.txt')
+        matrices_pathname = join(matrices_dirname, video_filename)
 
         matrices = homography_matrices(video)
 
